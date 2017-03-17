b55e142616ef01bc7c271c67038441ff4dccd9f4
Yang Lin <todoubaba@gmail.com>
2017-03-18 03:49:48 +0800

    Upgrade to spring cloud Camden.SR6 (#50)

--- tm-here-api/grails-app/conf/spring/resources.groovy
+++ tm-here-api/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,6 @@
-// Place your Spring DSL code here
 beans = {
+    // https://github.com/grails/grails-core/issues/640#issuecomment-286736679
+    springReactorEventTranslator(Dummy)
 }
+
+class Dummy {}
