29a7e76269e619ce252b3bd8ac7ee0461357cd14
todoubaba <todoubaba@gmail.com>
2017-05-17 03:52:31 +0800

    Interceptors

---
+++ tm-here-api/grails-app/controllers/cn/edu/bnuz/bell/here/RollcallInterceptor.groovy
@@ -0,0 +1,21 @@
+package cn.edu.bnuz.bell.here
+
+import cn.edu.bnuz.bell.security.SecurityService
+import org.springframework.http.HttpStatus
+
+class RollcallInterceptor {
+    SecurityService securityService
+
+    boolean before() {
+        if (params.teacherId != securityService.userId) {
+            render(status: HttpStatus.FORBIDDEN)
+            return false
+        } else {
+            return true
+        }
+    }
+
+    boolean after() { true }
+
+    void afterView() {}
+}
